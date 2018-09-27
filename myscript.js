95ced933e561c27ea8c2d6a089665501d6bb0fda
sondr0p <psndnb@gmail.com>
2018-09-27 10:35:27 -0700

    Added hover effect

--- myscript.js
+++ myscript.js
@@ -2,16 +2,26 @@ function createDivs(gridSize){
     var e = document.querySelector("#container")
     for(var i = 0; i < gridSize; i++){
         var row = document.createElement('div');
-        for(var j = 0; j < gridSize; j++){
+        for(var j = 1; j <= gridSize; j++){
             var cell = document.createElement('div');
             cell.style.backgroundColor = "lightgray";
             cell.style.border = "1px solid gray";
             cell.style.padding = "10px";
+            cell.addEventListener("mouseover", mouseOver);
+            //cell.addEventListener("mouseout", mouseOut);
             row.appendChild(cell);
         }
         row.style.float = 'left';
         e.appendChild(row);
     }
+}
+
+function mouseOver() {
+    this.style.backgroundColor = "blue";
+}
+
+function mouseOut() {
+    this.style.backgroundColor = "lightgray";
 }
 
 createDivs(16);
